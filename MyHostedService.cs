815f3ffd69f2d4f54e2757105dab5b54fc3276ee
Bharathkumar V <v.bharathkumar@gmail.com>
2020-06-28 19:09:47 +0530

    First Commit

---
+++ MyHostedService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace js.HostedService
+{
+    public class MyHostedService : IHostedService
+    {
+        private Timer _timer;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(MyTask, null, 0, 10000);
+            return Task.CompletedTask;
+        }
+
+        void MyTask(object state)
+        {
+            Debug.WriteLine("Executed My Task!!!");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+}
